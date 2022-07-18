b04c9f31e3c3630a10b633d0b0001413a8f58731
miguel-henrique <miguelhenrique.as10@gmail.com>
2022-07-18 02:02:55 -0300

    update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -15,7 +15,7 @@
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      let recaptcha = thisForm.getAttribute('data-sitekey');
       
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!')
