229bdcea69cc1f4146d46d0debacccacfe958aae
miguel-henrique <miguelhenrique.as10@gmail.com>
2022-07-18 17:40:00 -0300

    update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -79,7 +79,8 @@
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    thisForm.querySelector('.sent-message').classList.add('d-block');
+    window.location.replace("http://www.pluswebb.com");
   }
 
 })();
